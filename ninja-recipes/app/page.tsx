5c353b025ee3da2aac3e706bc730d3bf518e8672
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-03-07 22:33:59 +0900

    Finished ninja-recipes chapter 3

--- ninja-recipes/app/page.tsx
+++ ninja-recipes/app/page.tsx
@@ -1,3 +1,5 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -32,7 +34,12 @@ const HomePage = async () => {
         {recipes.map(recipe => (
           <Card key={recipe.id} className='flex flex-col justify-between'>
             <CardHeader className='flex-row gap-4 items-center'>
-              { /* avatar */}
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt='recipe img'/>
+                <AvatarFallback>
+                  {recipe.title.slice(0, 2)}
+                </AvatarFallback>
+              </Avatar>
               <div>
                 <CardTitle>{recipe.title}</CardTitle>
                 <CardDescription>{recipe.time} mins to cook.</CardDescription>
@@ -43,7 +50,7 @@ const HomePage = async () => {
             </CardContent>
             <CardFooter className='flex justify-between'>
               <Button>View Recipe</Button>
-              {recipe.vegan && <p>Vegan!</p>}
+              {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
             </CardFooter>
           </Card>
         ))}
